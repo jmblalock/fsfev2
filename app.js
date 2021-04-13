567ea30c7addfc48e807deb77912b4d45d8ecec1
jmblalock <justin.m.blalock@gmail.com>
2021-04-13 14:03:02 -0500

    custom header and status code

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ app.get('/', (req, res) => {
 
 });
 
+app.get('/demo', (req, res) => {
+  res.set('X-full-stack', '4life');
+  res.status(418);
+  res.send('I prefer coffee');
+})
+
 app.listen(port, () => console.log(`Listening on port: ${port}...`));
